4653dcc0b5d3a1a257b5eb11c2844420aac771be
sameer <maharjansameer111@gmail.com>
2022-08-22 16:06:41 +0545

    login req

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kqhm=6w(=gutrd#q==fsxdhk1ns$8pu5g*@ckcvqba)0sodo-n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
--- todo/views.py
+++ todo/views.py
@@ -12,19 +12,21 @@ def index(request):
     return render(request,'todo/index.html')
 
 # Create your views here.
+@login_required
 def todo_list(request):
     context =dict()
+    userid = request.user
     #for searching
     if 'q' in request.GET:
         q = request.GET['q']
         multiple_q = Q(Q(title__icontains=q) | Q(is_completed__icontains=q))
-        context["todos"] = Todo.objects.filter(multiple_q)
+        context["todos"] = Todo.objects.filter(multiple_q , user = userid)
     #end searching
     else:
-        context['todos'] = Todo.objects.all() #creating dictionary to pass all obj of Todo model
+        context['todos'] = Todo.objects.filter(user = userid) #creating dictionary to pass all obj of Todo model
     return render(request,"todo/todo_list.html",context)
 
-
+@login_required
 def todo_detail(request,pk):
     context = dict()
     try:
@@ -47,37 +49,16 @@ def todo_create(request):
         todo = form.save(commit=False)
         todo.user = request.user
         todo.save()
-    
-        # form.save()
-        # user = request.user.id
-        # print(user)
-        # todo=form.save(commit=False)
-        # todo.user = request.user.id
-        # todo.save()
-        # a.save()
-
         context["message"] = messages.success(request,"Inserted Successfully")
         return redirect("/")
     context['form'] = form
     return render(request,"todo/todo_create.html",context)
 
-# def todo_create(request):
-#     context = dict()
-#     if request.method == 'POST':
-#         form = TodoForm(request.POST)
-#     else:
-#         form = TodoForm()
-
-#     context = {
-#         'form' : form,
-#     }
-    
-#     return render(request,"todo/todo_create.html",context)
 
 
 
 
-
+@login_required
 def todo_delete(request,pk):
     context = dict()
     try:
@@ -88,7 +69,7 @@ def todo_delete(request,pk):
         context["message"] = messages.error("Item Doesnt Exist")
     return redirect("/")
 
-
+@login_required
 def todo_update(request, pk):
     context = dict()
     try:
