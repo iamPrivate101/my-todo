32e0ffdf718324d025765fdbd71589c3a0d5ddda
sameer <maharjansameer111@gmail.com>
2022-08-22 19:36:14 +0545

    index modified

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kqhm=6w(=gutrd#q==fsxdhk1ns$8pu5g*@ckcvqba)0sodo-n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -134,6 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/ 'staticfiles/'
+STATICFILES_DIRS = [(BASE_DIR/ 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
